0cecf3e046be4550c6697b3736acab1637b3cc13
Aiden Nibali <dismaldenizen@gmail.com>
2021-08-31 16:32:24 +1000

    Add CUDA PyTorch tests

--- tests/test_torch.py
+++ tests/test_torch.py
@@ -1,23 +1,51 @@
 import pytest
+import torch
+from pytest_lazyfixture import lazy_fixture
 
 from pybraw import PixelFormat, ResolutionScale
 from pybraw.torch.reader import FrameImageReader
 
 
 @pytest.fixture
-def reader_cpu(sample_filename):
-    return FrameImageReader(sample_filename, processing_device='cpu')
+def device_cpu():
+    return torch.device('cpu')
 
 
-def test_read_frames(reader_cpu):
+@pytest.fixture
+def device_cuda0():
+    if torch.cuda.is_available() and 0 < torch.cuda.device_count():
+        return torch.device('cuda:0')
+    pytest.skip('device not available (cuda:0)')
+
+
+@pytest.fixture
+def device_cuda1():
+    if torch.cuda.is_available() and 1 < torch.cuda.device_count():
+        return torch.device('cuda:1')
+    pytest.skip('device not available (cuda:1)')
+
+
+@pytest.fixture
+def reader_cpu(sample_filename, device_cpu):
+    return FrameImageReader(sample_filename, processing_device=device_cpu)
+
+
+@pytest.mark.parametrize('processing_device', [
+    lazy_fixture('device_cpu'),
+    lazy_fixture('device_cuda0'),
+    lazy_fixture('device_cuda1'),
+])
+def test_read_frames(sample_filename, processing_device):
+    reader = FrameImageReader(sample_filename, processing_device=processing_device)
     expected = [0.516379, 0.515850, 0.515255, 0.514853, 0.514609, 0.514260, 0.514031, 0.514378]
 
-    with reader_cpu.run_flow(PixelFormat.RGB_F32_Planar, max_running_tasks=3) as task_manager:
+    with reader.run_flow(PixelFormat.RGB_F32_Planar, max_running_tasks=3) as task_manager:
         tasks = [task_manager.enqueue_task(frame_index, resolution_scale=ResolutionScale.Eighth)
                  for frame_index in range(8)]
 
         for i, task in enumerate(tasks):
             image_tensor = task.consume()
+            assert image_tensor.device == processing_device
             assert float(image_tensor.mean()) == pytest.approx(expected[i], abs=1e-4)
 
 
@@ -40,3 +68,18 @@ def test_postprocessing_crop(reader_cpu):
         )
         image_tensor = task.consume()
     assert image_tensor.shape == (100, 200, 4)
+
+
+@pytest.mark.parametrize('out_device', [
+    lazy_fixture('device_cpu'),
+    lazy_fixture('device_cuda0'),
+    lazy_fixture('device_cuda1'),
+])
+def test_postprocessing_out_device(reader_cpu, out_device):
+    with reader_cpu.run_flow(PixelFormat.RGBA_U8_Packed, max_running_tasks=1) as task_manager:
+        task = task_manager.enqueue_task(
+            frame_index=0,
+            out_device=out_device,
+        )
+        image_tensor = task.consume()
+    assert image_tensor.device == out_device
